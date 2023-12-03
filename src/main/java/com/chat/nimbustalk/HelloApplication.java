9358444ba2edf013601220e91364b728f8604fbd
Haitam Elgharras <elgharras.haitam@gmail.com>
2023-12-03 21:34:05 +0100

    Adding Chat list

--- src/main/java/com/chat/nimbustalk/HelloApplication.java
+++ src/main/java/com/chat/nimbustalk/HelloApplication.java
@@ -5,14 +5,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        File homeFxml = new File("src/main/java/com/chat/nimbustalk/home.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(homeFxml.toURI().toURL());
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("NimbusTalk");
         stage.setScene(scene);
         stage.show();
     }
