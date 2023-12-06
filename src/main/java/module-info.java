0a2d36001cd2d5003684bd9e5b903edb3e117b0e
Haitam Elgharras <elgharras.haitam@gmail.com>
2023-12-06 18:29:57 +0100

    Home page designe

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,5 +5,7 @@ module com.nimbus.nimbustalk {
     requires org.kordamp.bootstrapfx.core;
 
     opens com.chat.nimbustalk to javafx.fxml;
-    exports com.chat.nimbustalk;
+    exports com.chat.nimbustalk.Server;
+    exports com.chat.nimbustalk.Client;
+    opens com.chat.nimbustalk.Client to javafx.fxml;
 }
