88e407a01043508c1dd67b95a81752e7bcce1693
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2023-12-19 22:53:47 +0100

    Changed DB provider

--- src/main/java/com/chat/nimbustalk/Server/dao/DBConnection.java
+++ src/main/java/com/chat/nimbustalk/Server/dao/DBConnection.java
@@ -7,10 +7,14 @@ public class DBConnection {
     private static Connection con;
     private static DBConnection c;
 
+    String jdbcUrl = "jdbc:postgresql://ep-wandering-unit-78681101.eu-central-1.postgres.vercel-storage.com:5432/verceldb";
+    String username = "default";
+    String password = "4CBf1boiqQhm";
+
     private DBConnection(){
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            this.con = DriverManager.getConnection("jdbc:mysql://localhost:3306/NimbusTalk","root","ilyas-2002");
+            Class.forName("org.postgresql.Driver");
+            this.con = DriverManager.getConnection(jdbcUrl, username,password);
         }
         catch (Exception e) {
             e.printStackTrace();
